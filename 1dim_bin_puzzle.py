1d2f07c1dd67dbef1f91d233158a40139018fb32
Huzairhzrm <62472509+Huzairhzrm@users.noreply.github.com>
2020-03-22 00:59:21 +0700

    Add files via upload

---
+++ 1dim_bin_puzzle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+puzzle = '_00_10_0_11__00_'
+#puzzle = '___00_010__10__'
+a = list(puzzle)
+change = True
+
+while change:
+    change = False
+    for i in range(1,len(a)-2):
+        #pair constraint
+        if a[i] == a[i+1] and a[i-1]  == '_':
+            if a[i] == '0':
+                a[i-1] = a[i+2] = '1'
+                change =True
+            elif a[i] == '1':
+                a[i-1] = a[i+2] = '0'
+                change =True
+        if a[i] == a[i+1] and a[i+2]  == '_':
+            if a[i] == '0':
+                a[i-1] = a[i+2] = '1'
+                change =True
+            elif a[i] == '1':
+                a[i-1] = a[i+2] = '0'
+                change =True
+
+        #trio constraint
+        if a[i] == a[i+2] and a[i+1] == '_':
+            if a[i] == '0':
+                a[i+1] = '1'
+                change =True
+            elif a[i] == '1':
+                a[i+1] == '0'
+                change =True
+
+puzzle, ''.join(a)
+
+
+# In[ ]:
+
+
+
+
